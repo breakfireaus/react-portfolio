1c42af012974275906cbc51556ff596081919641
breakfireaus <myoungerc@hotmail.com>
2023-02-21 19:08:32 +1100

    fixing links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
-import './FooterStyle.css'
-import { FaStackOverflow, FaGithub, FaLinkedin } from 'react-icons/fa'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import './FooterStyle.css';
+import { FaStackOverflow, FaGithub, FaLinkedin } from 'react-icons/fa';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
@@ -9,29 +9,32 @@ const Footer = () => {
       <div className='footer-container'>
         <div className='linkIcons'>
           <div className='socialmedia'>
-            <Link to='https://stackoverflow.com/users/21187328/matthew-younger-chudnovsky'>
+            <a
+              href='https://stackoverflow.com/users/21187328/matthew-younger-chudnovsky'
+              target='_blank'
+            >
               <FaStackOverflow
                 size={35}
                 style={{ color: 'white', marginRight: '1rem' }}
               />
-            </Link>
-            <Link to='https://github.com/breakfireaus'>
+            </a>
+            <a href='https://github.com/breakfireaus' target='_blank'>
               <FaGithub
                 size={35}
                 style={{ color: 'white', marginRight: '1rem' }}
               />
-            </Link>
-            <Link to='https://www.linkedin.com/in/matthew-yc/'>
+            </a>
+            <a href='https://www.linkedin.com/in/matthew-yc/' target='_blank'>
               <FaLinkedin
                 size={35}
                 style={{ color: 'white', marginRight: '1rem' }}
               />
-            </Link>
+            </a>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
